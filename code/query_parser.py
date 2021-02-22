e2f77a452c0a153888f51473c05c75e85851ac46
Anant <anantgupta04@gmail.com>
2021-02-23 01:54:18 +0530

    Query parser refactored

--- code/query_parser.py
+++ code/query_parser.py
@@ -130,21 +130,6 @@ Questions that can be answered?
 
 
 if __name__=="__main__":
-    # nl_query = input("Enter the query: ")
-
-    # win = Tk()# Application Name
-    # win.title("Python GUI App")# Label
-    # lbl = Label(win, text = "Enter your question:").grid(column = 0, row = 0)# Click event
-    # name = StringVar()
-
-    # def click():
-    #     print("Hi," + name.get())# Textbox widget
-
-    # nameEntered = Entry(win, width = 12, textvariable = name).grid(column = 0, row = 1)# Button widget
-    # button = Button(win, text = "submit", command = click).grid(column = 1, row = 1)
-    # win.mainloop()
-
-
     # Attempt 2
     root = Tk()
 
@@ -165,69 +150,20 @@ if __name__=="__main__":
 
     #Text for our form
     name = Label(root, text="Name")
-    # phone = Label(root, text="Phone")
-    # gender = Label(root, text="Gender")
-    # emergency = Label(root, text="Emergency Contact")
-    # paymentmode = Label(root, text="Payment Mode")
 
     #Pack text for our form
     name.grid(row=1, column=2)
-    # phone.grid(row=2, column=2)
-    # gender.grid(row=3, column=2)
-    # emergency.grid(row=4, column=2)
-    # paymentmode.grid(row=5, column=2)
 
     # Tkinter variable for storing entries
     namevalue = StringVar()
-    # phonevalue = StringVar()
-    # gendervalue = StringVar()
-    # emergencyvalue = StringVar()
-    # paymentmodevalue = StringVar()
-    # foodservicevalue = IntVar()
-
 
     #Entries for our form
     nameentry = Entry(root, textvariable=namevalue)
-    # phoneentry = Entry(root, textvariable=phonevalue)
-    # genderentry = Entry(root, textvariable=gendervalue)
-    # emergencyentry = Entry(root, textvariable=emergencyvalue)
-    # paymentmodeentry = Entry(root, textvariable=paymentmodevalue)
 
     # Packing the Entries
     nameentry.grid(row=1, column=3)
-    # phoneentry.grid(row=2, column=3)
-    # genderentry.grid(row=3, column=3)
-    # emergencyentry.grid(row=4, column=3)
-    # paymentmodeentry.grid(row=5, column=3)
-
-    # #Checkbox & Packing it
-    # foodservice = Checkbutton(text="Want to prebook your meals?", variable = foodservicevalue)
-    # foodservice.grid(row=6, column=3)
 
     #Button & packing it and assigning it a command
     Button(text="Submit your query", command=getNer).grid(row=7, column=3)
 
-
-
     root.mainloop()
-
-
-    '''
-    for i in ner:
-        if "where" in nl_query:
-            subject = ner[ner.index("formation")-1]
-            subject = subject + " formation"
-            # check whether we contain the certain formation or not
-            if subject.title() in formations:
-                # final_query = final_query + "select ?c " + "where {ns1:" + \
-                 subject.replace(" ","").lower()  +" ns1:located_in ?c}"
-                break
-            #print("Location required via query = \n",final_query)
-
-        elif "which" in nl_query:
-            if "group" in ner and "formation" in ner:
-
-                print("group-formation")
-            if "well" in ner and "formation" in ner:
-                print("well-formation")
-    '''
